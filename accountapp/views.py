e848c5fb67b7a3553020bb611df6aeb350a830f5
Woojin <dnjintaeni@naver.com>
2022-01-24 21:30:18 +0900

    djust django course 21 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 from accountapp.models import HelloWorld
+from django.views.generic import CreateView
 
 @csrf_exempt
 def hello_world(request):
@@ -17,9 +20,16 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        hello_world_list = HelloWorld.objects.all()
         return HttpResponseRedirect(reverse('accountapp:hello_world'))       
         #return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
